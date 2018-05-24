d3474946276c504b19e7ef600387c1b71586b420
manalipankaj <pankaj.manali@gmail.com>
2018-05-24 22:43:22 +0530

    Add timestamp as Id

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@
 
     function createColomn() {
         const addTaskButton = document.createElement("button");
-
         const newRowDiv = document.createElement("div");
+        
         newRowDiv.setAttribute("class","taskColomn");
         newRowDiv.innerHTML = "I am added";
         newRowDiv.setAttribute("draggable", "true");
@@ -20,13 +20,14 @@
         newRowDiv.addEventListener("drop",(ev) => {
             ev.preventDefault();
             var data = ev.dataTransfer.getData("text");
-
-            console.log(data);
-            ev.target.appendChild(document.getElementById(data))
+            ev.target.appendChild(document.getElementById(data));
+            ev.stopPropagation();
+            return;
         });
 
         newRowDiv.addEventListener("dragover", (ev) => {
             ev.preventDefault();
+            return;
         })
 
         newRowDiv.appendChild(addTaskButton);
@@ -34,18 +35,22 @@
 
         addTaskButton.addEventListener("click", () => {
             taskCounter++;
-            const id = Math.random();
+            const id = new Date().getTime();
             const newTaskElement = document.createElement("div");
+            
             newTaskElement.setAttribute("class", "task");
             newTaskElement.setAttribute("id",id)
             newTaskElement.setAttribute("draggable", "true");
+            
             newTaskElement.innerHTML = "task no "+taskCounter;
-            newRowDiv.appendChild(newTaskElement);
 
             newTaskElement.addEventListener("dragstart", (ev) => {
-                console.log("dragging");
                 ev.dataTransfer.setData("text", id);
-            })
+                ev.stopPropagation();
+                return;
+            });
+
+            newRowDiv.appendChild(newTaskElement);
         });
     }
 })();
